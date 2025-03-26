a41cc51b7335b9e185b772a50b9d539d6d6a5fb7
boubby <boubby.dsc@gmail.com>
2025-03-26 14:42:17 +0100

    26-03-2025

---
+++ Views/Components/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="footer-content">
+        <p>&copy; 2025. Tous droits réservés.</p>
+        <div class="social-links">
+            <a href="#" target="_blank" class="social-icon">Facebook</a>
+            <a href="#" target="_blank" class="social-icon">Twitter</a>
+            <a href="#" target="_blank" class="social-icon">Instagram</a>
+        </div>
+    </div>
+</footer>
---
+++ Views/Components/navBar.php
@@ -0,0 +1,14 @@
+<header class="header">
+    <div class="logo">
+        <a href="#">Campignions</a>
+    </div>
+    <nav class="nav">
+        <ul>
+            <li><a href="#">Lien</a></li>
+            <li><a href="#">Lien</a></li>
+            <li><a href="#">Lien</a></li>
+            <li><a href="#">Lien</a></li>
+            <li><a href="#">Lien</a></li>
+        </ul>
+    </nav>
+</header>
